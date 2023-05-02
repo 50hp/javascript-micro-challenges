029c01a77b4e9f1a3e1a46cb1164d0faf0f33606
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-02 17:55:16 -0500

    updated loopsArray funnction to be more consice.

--- scripts/LoopsArrayCon.js
+++ scripts/LoopsArrayCon.js
@@ -1,17 +1,10 @@
 function loopsArray( array , number){
-    let array2 = []
-    for (let i = number; i < array.length; i++){
-        array2.push(array[i]);
-        
-    }
-
-return array2.toString();
-
+    return array.slice(number, (array.length)).join(' ');
 }
 
 
 
-
+``
 
 result = loopsArray(["ASDF", "BLAH", "Oh!", "Hi", "There!"], 3);
 console.log(result);
